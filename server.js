6f0414eba72a9ac135b3306eea4f2d12e89cd037
g0tie <gaultierbegue@outlook.com>
2022-04-06 11:46:58 +0400

    finishing app demo main funcs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = require('express')();
 const connectToDb = require('./config/db.js');
 const bodyParser = require("body-parser");
 const UserModel = require("./models/User");
+const { redirect } = require('express/lib/response');
 
 connectToDb();
 app.use(bodyParser.json());
@@ -13,16 +14,55 @@ app.set('view engine', 'ejs');
 app.get('/', async (req, res) => {
 
     try {
-        let user = await UserModel.findOne({name: "Gaultier"});
-        res.render(__dirname + '/templates/index.ejs', {user: user.name, gender: user.genre, error:false});
+        let user = await UserModel.findOne({name: "Eric"});
+        res.render(__dirname + '/templates/index.ejs', {user: user.name, gender: user.genre, error:false, success: req.query.success || false});
     } catch (e) {
-        res.render(__dirname + '/templates/index.ejs', {error: e});
+        res.render(__dirname + '/templates/index.ejs', {error: e, success: false});
     }
 
 });
 
-app.get('/personne/:id', (req, res) => {
-    res.render(__diranme + '/templates/personnes/index.ejs', {name: req.params.id });
+app.get("/notfound", (req, res) => {
+    res.send("404 not found");
+});
+
+app.get('/personne/:id', async (req, res) => {
+    try {
+        let infos = await UserModel.findOne({name: req.params.id});
+
+        if (!infos) await res.redirect('/notfound');
+
+        res.render(__dirname + '/templates/personne/index.ejs', { name: infos.name, genre: infos.genre, success: req.query.success || false });
+    } catch (e) {
+        res.render(__dirname + '/templates/personne/index.ejs', {error: e, success: false});
+    }
+});
+
+app.post('/personne/:id', async (req, res) => {
+    try {
+        let user = await UserModel.findOne({name: req.params.id});
+        user.name = await req.body.username;
+        user.genre = await req.body.genre;
+
+        await user.save();
+
+        res.redirect(`/personne/${req.body.username}/?success=1`);
+    } catch (e) {
+        res.redirect(`/personne/${req.body.username}/?success=0`);
+    }
+});
+
+app.post('/personne', async (req, res) => {
+    try {
+        let newUser = UserModel.create({
+            name: req.body.username,
+            genre: req.body.genre
+        });
+
+        res.redirect('/?success=1');
+    } catch(e) {
+        res.redirect('/?success=0');
+    }
 });
 
 const listener = app.listen(process.env.PORT, () => console.log(`Server listening on port ${listener.address().port}`));
